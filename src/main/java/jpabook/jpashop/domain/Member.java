c4628f14af82f3fe299a06e74d4a5a597ad34d41
w-garden <w.garden316@gmail.com>
2022-12-13 23:28:17 +0900

    20221212 JPA 활용 2 - 회원수정API, 회원조회API

--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -17,12 +18,14 @@ public class Member {
     @Column(name="member_id")
     private Long id;
 
-    @NotEmpty  //엔티티에 검증로직이 들어가있음. 좋지 않음
+//    @NotEmpty  //엔티티에 검증로직이 들어가있음. 좋지 않음
     private String name;
 
     @Embedded //내장타입임을 명시
     private Address address;
 
+
+//    @JsonIgnore //엔티티에 검증로직이 들어가있는 것은 좋지 않음
     @OneToMany(mappedBy = "member") //일대다
     //mappedBy = "member" : 매핑된 값임을 명시. 읽기전용
     private List<Order> orders = new ArrayList<>();
